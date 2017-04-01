27385a0f506f32de2b4a9f88fd530776965a9d68
Peter <petoknm@gmail.com>
2017-04-01 22:44:38 +0200

    It actually works, lol

--- models/finger.ts
+++
@@ -1,4 +0,0 @@
-export class Finger {
-    extended: boolean;
-    joints: { name:string; bent: boolean }[];
-}
--- models/hand.ts
+++
@@ -1,8 +0,0 @@
-import {Finger} from "./finger";
-
-export class Hand {
-    orientation: { below: boolean };
-    palm: { facing: { front: boolean} };
-    fingers: Finger[];
-    fingersSeparated: { index: boolean; middle: boolean; };
-}
